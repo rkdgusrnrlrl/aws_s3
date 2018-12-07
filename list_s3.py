efa007e828339e37e12afe8c0d9fb02e6f71502a
rkgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-12-07 12:39:51 +0900

    Add script to show list of  busket

---
+++ list_s3.py
@@ -0,0 +1,21 @@
+import boto3
+import json
+
+with open('config.json') as f:
+    config = json.load(f)
+
+# Create an S3 client
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=config['aws_access_key_id'],
+    aws_secret_access_key=config['aws_secret_access_key']
+)
+
+# Call S3 to list current buckets
+response = s3.list_buckets()
+
+# Get a list of all bucket names from the response
+buckets = [bucket['Name'] for bucket in response['Buckets']]
+
+# Print out the bucket list
+print("Bucket List: %s" % buckets)
